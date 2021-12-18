afecb087755efad9ddd3e4d46ff54b84e00704e7
sangviehuy <hvsang2101@gmail.com>
2021-12-18 14:48:26 +0700

    unite test

---
+++ UserTest/AccountRepositoryTest.cs
@@ -0,0 +1,82 @@
+﻿using Entities;
+using Entities.Entities;
+using Entities.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Models;
+using Xunit;
+
+namespace UserTest
+{
+    public class AccountRepositoryTest : DbContextTest
+    {
+        public AccountRepositoryTest() : base(new DbContextOptionsBuilder<NALDbContext>()
+                       .UseInMemoryDatabase(databaseName: "NALDatabase")
+                       .Options)
+        {
+
+        }
+
+        [Fact]
+        public void GetUserByName_Password()
+        {
+            using (var context = new NALDbContext(ContextOptions))
+            {
+                // Arrange
+                var userExpected = new User { Id = 1, UserName = "admin", Password = "admin", UserType = "Admin" };
+                var accountRepo = new AccountRepository(context);
+
+                // Act
+                var userExist = accountRepo.GetByUsernameAndPassword(userExpected.UserName, userExpected.Password);
+                var userNull = accountRepo.GetByUsernameAndPassword("sa", "sa123");
+                // Assert
+                Assert.Null(userNull);
+                Assert.Equal(JsonConvert.SerializeObject(userExpected), JsonConvert.SerializeObject(userExist));
+            }
+        }
+
+        [Fact]
+        public void IsUserExistTest()
+        {
+            using (var context = new NALDbContext(ContextOptions))
+            {
+                // Arrange
+                var accountRepo = new AccountRepository(context);
+
+                // Act
+                var userExist = accountRepo.IsUserExist("admin");
+                var userNotExist = accountRepo.IsUserExist("admin2");
+
+                // Assert
+                Assert.True(userExist);
+                Assert.False(userNotExist);
+            }
+        }
+
+        [Fact]
+        public void CanRegisterUser()
+        {
+            using (var context = new NALDbContext(ContextOptions))
+            {
+                // Arrange
+                var accountRepo = new AccountRepository(context);
+                var newUser = new User { Id = 4, UserName = "user4", Password = "user4", UserType = "User" };
+
+                // Act
+                accountRepo.RegisterUser(newUser);
+
+                var getNewUser = accountRepo.GetByUsernameAndPassword(newUser.UserName, newUser.Password);
+
+                // Assert
+
+                Assert.Equal(JsonConvert.SerializeObject(newUser), JsonConvert.SerializeObject(getNewUser));
+            }
+        }
+    }
+}
---
+++ UserTest/DbContextTest.cs
@@ -0,0 +1,45 @@
+﻿using Entities;
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserTest
+{
+    public class DbContextTest
+    {
+        protected DbContextTest(DbContextOptions<NALDbContext> contextOption)
+        {
+            ContextOptions = contextOption;
+            Seed();
+        }
+
+        protected DbContextOptions<NALDbContext> ContextOptions { get; }
+
+        private void Seed()
+        {
+            using (var context = new NALDbContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.AddRange(lstUser);
+                context.SaveChanges();
+            }
+        }
+
+        private List<User> lstUser
+        {
+            get
+            {
+                return new List<User>
+                {
+                     new User { Id=1 ,UserName = "admin", Password = "admin", UserType = "Admin" },
+                     new User {Id=2 ,UserName = "user", Password = "user", UserType = "User" },
+                     new User {Id=3 ,UserName = "user1", Password = "user1", UserType = "User" },
+                };
+            }
+        }
+    }
+}
